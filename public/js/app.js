7dcfad0fa2aa9adfbb553673cee8434412394543
Edward Gao <egao@sent.com>
2017-02-15 09:18:10 -1000

    add links

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,21 @@
 console.log('sanity check');
+(function() {
+	const title = document.getElementsByClassName('title');
+	const bigCards = document.getElementsByClassName('big-card');
+	const smallCards = document.getElementsByClassName('small-card');
+	const linkTo = link => window.location.href = link;
+	const link = (group, link) => Array.prototype.forEach
+		.call(group, ele => {
+			ele.addEventListener('click', _ => {
+				switch(link) {
+					case 'card': linkTo(`/gallery/${ele.dataset.id}`);
+						break;
+					case 'home': window.location.href = '/';
+						break;
+				}
+			});
+		});
+	link(bigCards, 'card');
+	link(smallCards, 'card');
+	link(title, 'home');
+})();
