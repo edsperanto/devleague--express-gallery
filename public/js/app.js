322d3e427b0db317826925489ee10f1d846f35c1
Edward Gao <egao@sent.com>
2017-02-15 09:40:29 -1000

    shorten link code

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,15 @@
-console.log('sanity check');
 (function() {
 	const title = document.getElementsByClassName('title');
 	const bigCards = document.getElementsByClassName('big-card');
 	const smallCards = document.getElementsByClassName('small-card');
-	const linkTo = link => window.location.href = link;
-	const link = (group, link) => Array.prototype.forEach
-		.call(group, ele => {
-			ele.addEventListener('click', _ => {
-				switch(link) {
-					case 'card': linkTo(`/gallery/${ele.dataset.id}`);
-						break;
-					case 'home': window.location.href = '/';
-						break;
-				}
-			});
-		});
+	const redirTo = link => window.location.href = link;
+	const link = (group, link) => Array.prototype.forEach.call(group, ele => {
+		const dest = {
+			'home': '/',
+			'card': `/gallery/${ele.dataset.id}`
+		}
+		ele.addEventListener('click', _ => redirTo(dest[link]));
+	});
 	link(bigCards, 'card');
 	link(smallCards, 'card');
 	link(title, 'home');
