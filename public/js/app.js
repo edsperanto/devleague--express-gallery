6ae718715116b5c4cfcde4313c85534318b355fd
Edward Gao <egao@sent.com>
2017-02-16 00:34:58 -1000

    I put Javascript into an industrial DRYing machine

--- public/js/app.js
+++ public/js/app.js
@@ -1,48 +1,33 @@
 (function main() {
 
 	// get elements
-	const getClass = name => document.getElementsByClassName(name);
-	const getId = name => document.getElementById(name);
-	const title = getClass('title');
-	const newBtn = getId('new');
-	const editBtn = getId('edit');
-	const discardBtn = getId('discard');
-	const doneBtn = getId('done');
-	const bigCards = getClass('big-card');
-	const smallCards = getClass('small-card');
+	const [title, bigCards, smallCards] = ['title', 'big-card', 'small-card']
+		.map(card => document.getElementsByClassName(card));
+	const [newBtn, editBtn, discardBtn, doneBtn] = ['new', 'edit', 'discard', 'done']
+		.map(btn => document.getElementById(btn));
 
 	// make cards link
 	const redirTo = link => window.location.href = link;
+	const repeat = (f, groups) => groups.forEach(group => f.apply(this, group));
 	const link = (group, link) => Array.prototype.forEach.call(group, ele => {
-		const dest = {
-			'home': '/',
-			'card': `/gallery/${ele.dataset.id}`
-		}
+		const dest = { 'home': '/', 'card': `/gallery/${ele.dataset.id}` };
 		ele.addEventListener('click', _ => redirTo(dest[link]));
 	});
-	link(bigCards, 'card');
-	link(smallCards, 'card');
-	link(title, 'home');
+	repeat(link, [[bigCards, 'card'], [smallCards, 'card'], [title, 'home']]);
 
 	// control menu display
 	const url = window.location.pathname;
 	const onClick = (btn, cb) => btn.addEventListener('click', cb);
 	const show = btns => btns.forEach(btn => btn.parentElement.style.display = 'initial');	
-	const itemNewRE = new RegExp("^/gallery/new$", "g");
-	const itemRE = new RegExp("^\/gallery/[0-9]+$", "g");
-	const itemEditRE = new RegExp("^\/gallery/[0-9]+/edit$", "g");
+	const [itemRE, itemNewRE, itemEditRE] =
+		[/^\/gallery\/[0-9]+$/g, /^\/gallery\/new$/g, /^\/gallery\/[0-9]+\/edits$/g];
 	switch(true) {
-		case itemNewRE.test(url):
-			show([discardBtn, doneBtn]); break;
-		case itemRE.test(url):
-			show([newBtn, editBtn]); break;
-		case itemEditRE.test(url):
-			show([discardBtn, doneBtn]); break;
-		default:
-			show([newBtn]); break;
+		case itemNewRE.test(url): show([discardBtn, doneBtn]); break;
+		case itemRE.test(url): show([newBtn, editBtn]); break;
+		case itemEditRE.test(url): show([discardBtn, doneBtn]); break;
+		default: show([newBtn]); break;
 	}
-	onClick(newBtn, create);
-	onClick(editBtn, edit);
+	repeat(onClick, [[newBtn, create], [editBtn, edit], [discardBtn, discard], [doneBtn, done]]);
 
 	// button actions
 	function create() {
