151a5c564925fb820581fd46a46929c75bbca4e1
Edward Gao <egao@sent.com>
2017-03-04 14:51:00 -1000

    gulp watches gulpfile change

--- gulpfile.js
+++ gulpfile.js
@@ -4,31 +4,13 @@ const scss = require('gulp-sass');
 gulp.task('scss', _ => {
 	return gulp.src('./scss/app.scss')
 		.pipe(scss({ errLogToConsole: true }))
+		.on('error', process.exit)
 		.pipe(gulp.dest('./public/css'));
 });
 
-/*
-gulp.task('browserSync', _ => {
-	browserSync.init(['./public'], {
-		server: {
-			baseDir: './public'
-		}
-	});
-});
-*/
-
-/*
-gulp.task('server', cb => {
-	exec('nodemon server.js', (err, stdout, stderr) => {
-		console.log(stdout);
-		console.log(stderr);
-		cb(err);
-	});
-});
-*/
-
 gulp.task('watch', _ => {
 	gulp.watch('./scss/**/*.scss', ['scss']);
+	gulp.watch('./gulpfile.js', process.exit);
 });
 
-gulp.task('default', ['scss', 'watch']);
+gulp.task('default', ['watch']);
